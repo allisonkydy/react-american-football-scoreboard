defa429795a927a03b4333bccf6d25b13b8681d5
allisonkydy <allisnelly@gmail.com>
2019-08-12 16:42:38 -0500

    add buttons and state for ballOn and toGo

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   const [awayScore, setAwayScore] = useState(0);
   const [down, setDown] = useState(1);
   const [quarter, setQuarter] = useState(1);
+  const [ballOn, setBallOn] = useState(50);
+  const [toGo, setToGo] = useState(10);
 
   // handlers
   const handleScore = (team, points) => {
@@ -18,6 +20,16 @@ function App() {
   const handleDownQuarter = (element, setter) => {
     if (element < 4) setter(element + 1);
     else if (element === 4) setter(1);
+  }
+
+  const handleBallOn = () => {
+    if (ballOn > 1) setBallOn(ballOn - 1);
+    else if (ballOn === 1) setBallOn(50);
+  }
+
+  const handleToGo = () => {
+    if (toGo > 1) setToGo(toGo - 1);
+    else if (toGo === 1) setToGo(10);
   }
 
   return (
@@ -34,7 +46,7 @@ function App() {
             <div className="away__score">{awayScore}</div>      
           </div>
         </div>
-        <BottomRow down={down} quarter={quarter}/>
+        <BottomRow down={down} quarter={quarter} ballOn={ballOn} toGo={toGo}/>
       </section>
       <section className="buttons">
         <div className="homeButtons">
@@ -48,6 +60,8 @@ function App() {
         <div>
           <button className="bottomButtons" onClick={() => handleDownQuarter(down, setDown)}>Next Down</button>
           <button className="bottomButtons" onClick={() => handleDownQuarter(quarter, setQuarter)}>Next Quarter</button>
+          <button className="bottomButtons" onClick={() => handleBallOn()}>Move Ball</button>
+          <button className="bottomButtons" onClick={() => handleToGo()}>Change To Go</button>
         </div>
       </section>
     </div>
