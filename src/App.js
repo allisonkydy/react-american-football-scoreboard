2b3d630466f64dd2860bc4ce93b5dd1465790b1b
allisonkydy <allisnelly@gmail.com>
2019-08-12 17:22:13 -0500

    factored out ScoreButtons into separate componenet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import TopRow from "./TopRow";
 import BottomRow from "./BottomRow";
+import ScoreButtons from "./ScoreButtons";
 
 function App() {
    // set state
@@ -40,14 +41,7 @@ function App() {
         <BottomRow down={down} quarter={quarter} ballOn={ballOn} toGo={toGo}/>
       </section>
       <section className="buttons">
-        <div className="homeButtons">
-          <button className="homeButtons__touchdown" onClick={() => handleScore("home", 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => handleScore("home", 3)}>Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => handleScore("away", 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => handleScore("away", 3)}>Away Field Goal</button>
-        </div>
+        <ScoreButtons handleScore={handleScore}/>
         <div>
           <button className="bottomButtons" onClick={() => handleDownQuarter(down, setDown)}>Next Down</button>
           <button className="bottomButtons" onClick={() => handleDownQuarter(quarter, setQuarter)}>Next Quarter</button>
@@ -58,5 +52,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
